3e09f02155a70bf9e26bd10caefe9a9ae2a0668c
Livia <livia_sorostinean@yahoo.com>
2022-02-25 17:28:07 +0200

    Change image name

--- BooksApp/BooksApp/View/TableView.swift
+++ BooksApp/BooksApp/View/TableView.swift
@@ -6,7 +6,7 @@ struct TableView: View {
                  Book (title: "Ion", author: "Rebreanu", year: 1959, image: "Ion", price: 12.7,rating: 4, link: "https://carturesti.ro"),
                  Book (title: "Poezii", author: "Bacovia", year: 1933, image: "Poezii", price: 15.8,rating:2, link: "https://carturesti.ro"),
                  Book (title: "Temă pentru acasă", author: "Dabija", year: 2019, image: "Dabija", price: 20.6,rating:5, link: "https://carturesti.ro"),
-                 Book (title: "Adam și Eva", author: "Rebreanu", year: 1946, image: "Adam și Eva", price: 12.8,rating:3, link: "https://carturesti.ro"),
+                 Book (title: "Adam și Eva", author: "Rebreanu", year: 1946, image: "Adam si Eva", price: 12.8,rating:3, link: "https://carturesti.ro"),
                  Book (title: "Fecioarele", author: "Michaelides", year: 2021, image: "Fecioarele", price: 30.5,rating:4, link: "https://carturesti.ro")]
     
     var body: some View {
